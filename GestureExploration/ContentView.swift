c7fe3e421c8ab36eaf188aab30b1fda91039000b
edy alves <30.edyalves@gmail.com>
2024-11-02 14:08:31 -0300

    Initial Commit

---
+++ GestureExploration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GestureExploration
+//
+//  Created by ednardo alves on 02/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GestureExploration/GestureExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureExplorationApp.swift
+//  GestureExploration
+//
+//  Created by ednardo alves on 02/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GestureExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
